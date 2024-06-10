b8c3056afb452881c482af64c9ab5f91ea2b63e8
cheeseNA <kurocat2000@gmail.com>
2024-06-10 18:02:52 +0900

    Add Create button (temp)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import { Button, useDisclosure } from "@nextui-org/react";
 import { TaskTable } from "@/components/TaskTable";
+import { CreateTaskModal } from "@/components/CreateTaskModal";
+
 export default function Home() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
     <main>
-      <div>
+      <div className="flex flex-col">
+        <div className="py-4 flex justify-between items-center">
+          <p className="font-semibold text-inherit text-lg">Tasks</p>
+          <Button onPress={onOpen} color="primary">
+            Create
+          </Button>
+        </div>
         <TaskTable />
+        <CreateTaskModal isOpen={isOpen} onOpenChange={onOpenChange} />
       </div>
     </main>
   );
