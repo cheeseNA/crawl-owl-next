2b33e957fd7a2bd96aa877f0d9e36fb3f0911730
cheeseNA <kurocat2000@gmail.com>
2024-06-18 23:58:45 +0900

    Update api.ts to set Bearer

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,6 @@
 import createClient, { type Middleware } from "openapi-fetch";
 import type { paths } from "./schema";
+import { auth } from "@/firebase/firebase";
 
 const throwOnError: Middleware = {
   async onResponse(res) {
@@ -13,10 +14,36 @@ const throwOnError: Middleware = {
   },
 };
 
+const fetchRequestInterceptor: Middleware = {
+  async onRequest(req, _options) {
+    if (auth.currentUser === null) {
+      return req;
+    }
+    const token = await auth.currentUser.getIdToken();
+    if (!token) {
+      return req;
+    }
+    req.headers.set("Authorization", `Bearer ${token}`);
+    return req;
+  },
+};
+
+const fetchResponseInterceptor: Middleware = {
+  async onResponse(res, _options) {
+    if (res.status === 401) {
+      console.log("Sign out because of 401");
+      await auth.signOut();
+    }
+    return res;
+  },
+};
+
 const client = createClient<paths>({
   baseUrl: process.env.NEXT_PUBLIC_BACKEND_API_URL,
 });
 
 client.use(throwOnError);
+client.use(fetchRequestInterceptor);
+client.use(fetchResponseInterceptor);
 
 export default client;
